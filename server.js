9634cc96a3107ae93917d76704661710e57918c7
Kathryn Helsley <kathrynhelsley@Kathryns-MBP-2.frontierlocal.net>
2019-04-17 11:56:04 -0700

    added links from home index.html page

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 var express = require("express");
 var path = require("path");
+var serve = require('express-static');
+
 
 var app = express();
 var PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(path.join(__dirname + "/assets")));
+app.use(express.static(path.join(__dirname + "/public")));
  
 console.dir(app);   
 var reservations = [
